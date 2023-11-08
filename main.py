8f9afa9e858ca9314deba7c1da70b01b14efe1aa
Yahton <bekbulatovazrael@gmail.com>
2023-11-08 22:42:42 +0300

    wavelink using

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import logging
 import discord
 from discord.ext import commands
 
+import wavelink
+
 from admin import AdminCommands
 from config import TOKEN
 from bot_commands import CustomCommands
@@ -19,7 +21,7 @@ class Pentabot(commands.Bot):
         super().__init__(command_prefix="!", intents=intents)
 
     async def on_ready(self) -> None:
-        print(f'We have logged in as {bot.user}')
+        print(f'We have logged in as {self.user} | {self.user.id}')
         await self.add_cog(CustomCommands())
         print(f'The CustomCommands cog has been initialized')
         await self.add_cog(VoiceCommands())
@@ -33,6 +35,18 @@ class Pentabot(commands.Bot):
         # do some extra stuff here
 
         await self.process_commands(message)
+
+    async def setup_hook(self) -> None:
+        # Wavelink 2.0 has made connecting Nodes easier... Simply create each Node
+        # and pass it to NodePool.connect with the client/bot.
+        node1: wavelink.Node = wavelink.Node(uri='lava1.horizxon.tech:443', password='horizxon.tech', secure=True, retries=3)
+        node2: wavelink.Node = wavelink.Node(uri='lava2.horizxon.tech:443', password='horizxon.tech', secure=True, retries=3)
+        node3: wavelink.Node = wavelink.Node(uri='lava3.horizxon.tech:443', password='horizxon.tech', secure=True, retries=3)
+        node4: wavelink.Node = wavelink.Node(uri='suki.nathan.to:443', password='adowbongmanacc', secure=True, retries=3)
+        node5: wavelink.Node = wavelink.Node(uri='oce-lavalink.lexnet.cc:443', password='lexn3tl@val!nk', secure=True, retries=3)
+        node6: wavelink.Node = wavelink.Node(uri='lavalink.ordinaryender.my.eu.org:443', password='ordinarylavalink', secure=True, retries=3)
+        node7: wavelink.Node = wavelink.Node(uri='lavalink.justapie.net:443', password='pieajust12@XyZ', secure=True, retries=3)
+        await wavelink.NodePool.connect(client=self, nodes=[node7])
 
 
 if __name__ == "__main__":
--- voice.py
+++ voice.py
@@ -1,71 +1,34 @@
-import os
-
-import discord
-from discord.ext import tasks, commands
-
-from music import download_audio, search, string2link
+from discord.ext import commands
+import wavelink
 
 
 class VoiceCommands(commands.Cog):
     def __init__(self):
-        # self.bot = bot
-        self.is_playing = False
-        self.paused = False
-        # self.is_playing = False
-        self.channel = None
-        self.vc = None
+        pass
 
     @commands.command()
-    async def join(self, ctx: commands.Context):
-        if not ctx.author.voice:
-            await ctx.reply("пожалуйста, зайдите на канал!", mention_author=False)
+    async def play(self, ctx: commands.Context, search: str) -> None:
+        """Simple play command."""
+
+        if not ctx.voice_client:
+            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        else:
+            vc: wavelink.Player = ctx.voice_client
+
+        tracks = await wavelink.YouTubeTrack.search(search)
+        if not tracks:
+            await ctx.reply(f'По запросу: `{search}` треков не найдено ;(', mention_author=False)
             return
-        self.channel = ctx.author.voice.channel
-        self.vc = await self.channel.connect(self_deaf=True)
+
+        track = tracks[0]
+        await vc.play(track)
+        await ctx.reply(f"Сейчас играет: {track}", mention_author=False)
 
     @commands.command()
-    async def leave(self, ctx: commands.Context):
-        if self.channel is not None:
-            await self.vc.disconnect()
-            self.channel = None
-            await ctx.reply(f"я отключился от голосового канала!", mention_author=False)
+    async def disconnect(self, ctx: commands.Context) -> None:
+        """Simple disconnect command.
 
-    @commands.command()
-    async def play(self, ctx: commands.Context, link: str = None):
-        if link is None:
-            await ctx.reply("пожалуйста, укажите ссылку или название!", mention_author=False)
-            return
-        if "http" not in link:
-            link = await string2link(link)
-        if ctx.author.voice.channel != self.channel:
-            await self.join(ctx)
-        video_title = None
-        file_name = f"data/{ctx.guild.id}/{hash(link)}.m4a"
-        if not os.path.isfile(file_name):
-            video_title = download_audio(link, ctx.guild.id)
-        if self.is_playing:
-            self.vc.stop()
-            self.is_playing = False
-        self.vc.play(discord.FFmpegPCMAudio(file_name))
-        self.is_playing = True
-        await ctx.reply(f'Сейчас играет: "{video_title}"', mention_author=False)
-
-    @commands.command()
-    async def search(self, ctx: commands.Context, *args):
-        name = ' '.join(args)
-        search_result = search(name, 5)
-        msg = '\n'.join(f"Название: {video['title']}\nСсылка: {'https://www.youtube.com' + video['url_suffix']}" for video in search_result)
-        await ctx.reply(msg, mention_author=False)
-
-    @commands.command()
-    async def pause(self, ctx: commands.Context):
-        if not self.paused:
-            self.vc.pause()
-            self.paused = True
-
-    @commands.command()
-    async def resume(self, ctx: commands.Context):
-        if self.paused:
-            self.vc.resume()
-            self.paused = False
-
+        This command assumes there is a currently connected Player.
+        """
+        vc: wavelink.Player = ctx.voice_client
+        await vc.disconnect()
